682ed82b6e73fa7fa78b4d8e8fd062a098a6a832
nickryanweber <nicholasryanweber@gmail.com>
2019-09-18 22:08:11 -0400

    added all endpoints

--- Controllers/ItemController.cs
+++ Controllers/ItemController.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using storeinventory;
+using StoreInventory.Models;
 
 namespace StoreInventory.Controllers
 {
@@ -13,5 +16,74 @@ namespace StoreInventory.Controllers
     {
       this.context = _context;
     }
+
+    [HttpPost]
+    public ActionResult<Item> CreateItem([FromBody]Item item)
+    {
+      context.Items.Add(item);
+      context.SaveChanges();
+      return item;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Item>> GetAllItems()
+    {
+      var items = context.Items.OrderByDescending(item => item.DateOrdered);
+      return items.ToList();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult GetOneItem(int id)
+    {
+      var singleItem = context.Items.FirstOrDefault(item => item.Id == id);
+      if (singleItem == null)
+      {
+        return NotFound();
+      }
+      else
+      {
+        return Ok(singleItem);
+      }
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult<Item> UpdateItem([FromBody]Item updatedInfo, int id)
+    {
+      var updateItem = context.Items.FirstOrDefault(item => item.Id == id);
+      if (updateItem == null)
+      {
+        return NotFound();
+      }
+      else
+      {
+        updateItem.SKU = updatedInfo.SKU;
+        updateItem.Name = updatedInfo.Name;
+        updateItem.Description = updatedInfo.Description;
+        updateItem.NumberInStock = updatedInfo.NumberInStock;
+        updateItem.Price = updatedInfo.Price;
+        updateItem.DateOrdered = updatedInfo.DateOrdered;
+        context.SaveChanges();
+        return Ok(updateItem);
+      }
+
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult DeleteItem(int id)
+    {
+      var deletedItem = context.Items.FirstOrDefault(item => item.Id == id);
+      if (deletedItem == null)
+      {
+        return NotFound();
+      }
+      else
+      {
+        context.Remove(deletedItem);
+        context.SaveChanges();
+        return Ok(deletedItem);
+      }
+    }
+
+
   }
 }
